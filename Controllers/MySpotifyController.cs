ffd4cb5e1eaeefceeb6c2664940d6a96390d8186
Frederick Lopez <mtester010@gmail.com>
2022-07-10 17:55:27 -0500

    Added user collection page where you can select for a user and display their collection of musics

--- Controllers/MySpotifyController.cs
+++ Controllers/MySpotifyController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SD_310_W22SD_Assignment.Models;
 
 namespace SD_310_W22SD_Assignment.Controllers
 {
     public class MySpotifyController : Controller
     {
+        private MySpotifyDBContext _db { get; set; }
+
+        public MySpotifyController(MySpotifyDBContext context)
+        {
+            _db = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult UserCollection(int? id)
+        {
+            SelectList userList = new SelectList(_db.Users, "Id", "Name");
+            ViewBag.UserList = userList;
+            if (id.HasValue)
+            {
+                User currentUser = _db.Users.First(u => u.Id == id);
+                return View(_db.Collections.Where(c => c.UserId == currentUser.Id).Include(c => c.Music).ThenInclude(m => m.Song).Include(m => m.Music).ThenInclude(m => m.Artist));
+            } else
+            {
+                return View();
+            }
+                        
+        }
+
     }
 }
